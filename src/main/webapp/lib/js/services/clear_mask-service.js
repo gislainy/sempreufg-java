a2a86219bbf4f1f6a8a3b7a8020fea5092edb45c
Tony William <tonywmfreitas2@gmail.com>
2017-05-26 01:36:04 -0300

    Serviço para limpar mascaras

---
+++ src/main/webapp/lib/js/services/clear_mask-service.js
@@ -0,0 +1,22 @@
+(function(){
+    angular.module('sportsgo').service('clearMaskService', clearMaskService);
+
+    function clearMaskService(){
+        this.clearMaskCpf = function(cpf) {
+            return cpf.replace('.', '').replace('.', '').replace('-', '').trim();
+        }; 
+        this.clearMaskCnpj = function(cnpj) {
+            return cnpj.replace('.','').replace('.','').replace('/','').replace('-','').trim();
+        };
+        this.clearMaskValor = function(valor) {
+            return valor.replace('.', '').replace(',', '').replace('R$', '').trim();
+        };
+        this.clearMaskCep = function(cep) {
+            return cep.replace('.','').replace('-','').trim();
+        };
+        this.clearMaskTelefone = function(telefone) {
+            return telefone.replace('(','').replace(')','').replace('-','').trim();
+        }
+    }
+
+})();
